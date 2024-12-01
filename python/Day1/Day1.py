bd5f162bd73cafd8efff0772433df12bb86ef29a
Jeremy M. G. Leung <jml230@pitt.edu>
2024-12-01 12:27:45 -0500

    start of 2024 advent calendar

---
+++ python/Day1/Day1.py
@@ -0,0 +1,67 @@
+# Day1.py
+#
+# First attempt at doing Day 1 of Advent of Code 2023
+
+import time
+import numpy
+
+
+def read_input(file_name):
+    with open(file_name, 'r') as f:
+        read_list = f.read().splitlines()
+
+    final_count = [0]
+    counter = 0
+    for entry in read_list:
+        if entry != '':
+            final_count[counter] += int(entry)
+        else:
+            counter += 1
+            final_count.append(0)
+
+    final_count = numpy.asarray(final_count)
+
+    return final_count
+
+
+def return_max(input_list):
+    max_cal = numpy.max(input_list)
+    where = numpy.where(input_list == max_cal)[0][0]
+
+    return [int(max_cal), where]
+
+
+def return_top3(input_list):
+    sorted_list = sorted(input_list, key=lambda x: -x)
+    cal_sum = sum(sorted_list[0:3])
+    where_three = numpy.where(input_list > sorted_list[3])[0]
+
+    return [int(cal_sum), where_three]
+
+
+def main():
+    ## Part 1
+    b = read_input("Day1_test_input.txt")
+    # b = read_input("Day1_input.txt")
+    answer = return_max(b)
+    print(f'Elf {answer[1]+1} has {answer[0]} calories worth of food.')
+
+
+def main2():
+    # Part 2
+    a = read_input("Day1_test_input.txt")
+    # a = read_input("Day1_input.txt")
+    answer2 = return_top3(a)
+    print(f'Elves {answer2[1] + 1} have {answer2[0]} calories worth of food.')
+
+
+if __name__ == "__main__":
+    ## Part 1
+    startTime = time.perf_counter()
+    main()
+    print(f'{time.perf_counter() - startTime} sec.')
+
+    ## Part 2
+    # startTime = time.perf_counter()
+    # main2()
+    # print(f'{time.perf_counter() - startTime} sec.')
---
+++ python/new_day.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# new_day.sh
+# Quick Function to create a new day.
+#
+
+cp -rf DayX Day"$@" &&
+cd Day"$@" &&
+sed -e "s/DayX/Day$@/g" -e "s/Day X/Day $@/g" DayX.py > Day"$@".py &&
+rm DayX.py &&
+curl https://adventofcode.com/2024/day/$@/input --cookie "session=$(cat ../../cookies.txt)" -o Day"$@"_input.txt
