4d2beed78f3106577b07d4a59cd5a9ae143244dc
Jeremy M. G. Leung <jml230@pitt.edu>
2024-12-08 16:20:55 -0500

    update new_day

--- python/new_day.sh
+++ python/new_day.sh
@@ -6,5 +6,6 @@
 cp -rf DayX Day"$@" &&
 cd Day"$@" &&
 sed -e "s/DayX/Day$@/g" -e "s/Day X/Day $@/g" DayX.py > Day"$@".py &&
+mv DayX_test_input.txt Day"$@"_test_input.txt &&
 rm DayX.py &&
 curl https://adventofcode.com/2024/day/$@/input --cookie "session=$(cat ../../cookies.txt)" -o Day"$@"_input.txt
